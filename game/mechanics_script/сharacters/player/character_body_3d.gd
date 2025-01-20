fada2a3a2e7bfa8714c4783f02ead05130844f27
Covr2000 <51473511+Covr2000@users.noreply.github.com>
2025-01-20 20:44:55 +0300

    Динамичная камера и работа с бегом

--- game/mechanics_script/сharacters/player/character_body_3d.gd
+++ game/mechanics_script/сharacters/player/character_body_3d.gd
@@ -14,7 +14,13 @@ func _ready():
 	pass
 
 
-var rot = -0.02
+
+#fov run
+const FOV = 70 # потом брать из настроек
+const FOV_RUN_OPT =0.5 # На сколько увеличивать при беге
+
+# sensivite
+var rot = -0.01
 var rot_x = 0 
 var rot_y = 0
 
@@ -26,13 +32,22 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		if (rot_y + event.relative.y * rot) + 0.2 < 1 and (rot_y + event.relative.y * rot) - 0.2> -1:
 			rot_y += event.relative.y * rot
-			
+		
 		rot_x += event.relative.x * rot	
+		#print('rot_x sin: ', rot_x * sin(rot_x * 0.7))
+		#print('rot_y: ', rot_y)
+		
 		transform.basis = Basis(Vector3(0,1,0), rot_x)
 		$Camera3D.transform.basis = Basis(Vector3(1,0,0), rot_y)
 		$SpotLight3D.transform.basis = Basis(Vector3(1,0,0), rot_y)
+		
+		#rot_x += event.relative.x * rot	
+		#transform.basis = Basis(Vector3(0,1,0), rot_x)
+		#$Camera3D.transform.basis = Basis(Vector3(1,0,0), rot_y)
+		#$SpotLight3D.transform.basis = Basis(Vector3(1,0,0), rot_y)
 	
 	
+var bt = 0.0
 	
 func _physics_process(delta):
 
@@ -50,8 +65,8 @@ func _physics_process(delta):
 			SPEED += BOOST
 	else:
 		SPEED = MIN_SPEED
-		
-		
+
+
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -65,5 +80,23 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	
+	velocity.x = velocity.x 
+	velocity.y = velocity.y
+	
+	bt = bt + delta * velocity.length() * float(is_on_floor())
+	$Camera3D.transform.origin = sinCamera(bt)
+	
+	#fov_run_change
+	var vel_cl = clamp(velocity.length(), 0.5, SPEED)
+	var target_fov = FOV - FOV_RUN_OPT * vel_cl
+	$Camera3D.fov = lerp($Camera3D.fov, target_fov, delta * 1.0)
 	move_and_slide()
+
+func sinCamera(bt):
+	var pos = Vector3.ZERO
+	# 1.9 высота над камерой 
+	pos.y = 1.9 + (sin(bt * 1.0) * 0.05)
+	pos.x = cos(bt * 1.0 / 2) * 0.05
+	return pos
