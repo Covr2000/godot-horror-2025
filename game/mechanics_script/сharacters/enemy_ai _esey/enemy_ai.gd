2d992b98b7aed6a48e6b1bf508bea1431747f7fd
alex_home <namealex.dev@gmail.com>
2025-01-25 21:03:20 +0300

    move random

--- game/mechanics_script/сharacters/enemy_ai _esey/enemy_ai.gd
+++ game/mechanics_script/сharacters/enemy_ai _esey/enemy_ai.gd
@@ -5,6 +5,30 @@ extends CharacterBody3D
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
-func _physics_process(_delta):
-	velocity = velocity.rotated(Vector3.UP, rotation.y)
-	move_and_slide()
+# Скорость движения вперед
+@export var speed: float = 5.0
+
+var direction = Vector3.BACK.normalized()
+var tracking = false
+
+# Генерация случайного направления
+func get_random_direction_2d() -> Vector3:
+	var angle = randf_range(0, 2 * PI)  # Случайный угол в радианах
+	return Vector3(cos(angle), 0, sin(angle)).normalized()
+
+func _physics_process(delta: float) -> void:
+	look_at(global_position + direction, Vector3.UP)
+	velocity = -direction * speed
+	var collision = move_and_slide()
+	print(collision)
+	if collision:
+		direction = get_random_direction_2d()
+		#движение в другую сторону
+		#tracking = !tracking
+		#
+	#if tracking:
+		#direction = Vector3.FORWARD.normalized()
+	#else:
+		#direction = Vector3.BACK.normalized()
+
+	
