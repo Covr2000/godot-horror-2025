955c6303abfc5696e3f4af2401d272854fc4116f
Covr2000 <51473511+Covr2000@users.noreply.github.com>
2025-01-27 21:15:08 +0300

    sp KAT TEST ON REDY

--- game/scene_game/level/levl_1/global_level_1.gd
+++ game/scene_game/level/levl_1/global_level_1.gd
@@ -1,10 +1,24 @@
 extends Node3D
 
-
+@onready var a :AnimationPlayer = $katTest/Camera3D/AnimationPlayer
+var local_anim_on = false
 
 
 func _ready():
 	#print($palyer.transform.origin)
 	GlobalPlayer.GPlayerPosition_1 = $palyer.transform.origin 
+	a.play("kat")
+	
+	
+	
+	
+func _process(_delta):
+	if local_anim_on == false:
+		print(a.is_playing())
+		if a.is_playing() != true:
+			$katTest.queue_free()
+			local_anim_on = true
+	pass
+	
 	
 	# получить все лампочки и видны ли они или нет
